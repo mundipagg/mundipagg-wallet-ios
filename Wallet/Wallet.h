e29042532ac02a5ad4bc1949216f06b89be69279
Tatiana Magdalena <tmagdalena@stone.com.br>
2016-09-20 17:04:06 -0300

    Initial commit

---
+++ Wallet/Wallet.h
@@ -0,0 +1,19 @@
+//
+//  Wallet.h
+//  Wallet
+//
+//  Created by Tatiana Magdalena on 9/20/16.
+//  Copyright © 2016 Buy4. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Wallet.
+FOUNDATION_EXPORT double WalletVersionNumber;
+
+//! Project version string for Wallet.
+FOUNDATION_EXPORT const unsigned char WalletVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Wallet/PublicHeader.h>
+
+
