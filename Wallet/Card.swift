ea63a5a964160b6fa4f884683634ab6412f91a61
Jaime Costa Marques <jaime.mqu@gmail.com>
2016-11-11 22:09:07 -0200

    Finaliza criação do metodo de listagem de cartão

--- Wallet/Card.swift
+++ Wallet/Card.swift
@@ -44,7 +44,7 @@ public struct Card {
 	
 	
 	
-	public init?(id: String,
+	/*public init?(id: String,
 	            gatewayId: String? = nil,
 	            lastFourDigits: String? = nil,
 	            brand: CardBrand? = nil,
@@ -62,9 +62,10 @@ public struct Card {
 		self.createdAt = createdAt
 		self.updatedAt = updatedAt
 		self.billingAddress = billingAddress
-	}
+	}*/
 	
-	public init?(fromJSON jsonData: Data){
+	/*public init?(fromJSON jsonData: Data){
+		
 		
 		guard let json = (try? JSONSerialization.jsonObject(with: jsonData, options: [])) as? [String: Any],
 			let id = json["id"] as? String,
@@ -90,30 +91,40 @@ public struct Card {
 		self.createdAt = createdAt
 		self.updatedAt = updatedAt
 		self.billingAddress = Address()
-	}
+	}*/
 	
     public init?(fromDictionary json: Dictionary<String,Any>) {
-        
-        guard let id = json["id"] as? String,
-            let gatewayId = json["gateway_id"] as? String,
-            let lastFourDigits = json["lastFourDigits"] as? String,
-            let brand = json["brand"] as? String,
-            let status = json["status"] as? String,
-            let holderName = json["holderName"] as? String,
-            let createdAt = (json["createdAt"] as? String)?.dateFromISO8601Format,
-            let updatedAt = (json["updatedAt"] as? String)?.dateFromISO8601Format,
-            let billingAddress = json["billingAddress"] as? AnyObject
-            else { return nil }
-        
-        self.id = id
-        self.gatewayId = gatewayId
-        self.lastFourDigits = lastFourDigits
-        self.brand = CardBrand(rawValue: brand)
-        self.status = CardStatus(rawValue: status)
-        self.holderName = holderName
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        self.billingAddress = Address()
+		
+		
+		
+		guard let id = json["id"] as? String,
+			let gatewayId = json["gateway_id"] as? String,
+			let lastFourDigits = json["last_four_digits"] as? String,
+			let brand = json["brand"] as? String,
+			let status = json["status"] as? String,
+			let holderName = json["holder_name"] as? String,
+			let createdAt = (json["created_at"] as? String)?.dateFromISO8601Format,
+			let updatedAt = (json["updated_at"] as? String)?.dateFromISO8601Format
+		else {
+			return nil
+		}
+		
+		// Optional billing address must be outside guard statement
+		if let billingAddress = json["billing_address"] as? AnyObject {
+			print("Good Billing Address", billingAddress)
+		}else{
+			print("Bad Billing Address")
+		}
+		
+		self.id = id
+		self.gatewayId = gatewayId
+		self.lastFourDigits = lastFourDigits
+		self.brand = CardBrand(rawValue: brand)
+		self.status = CardStatus(rawValue: status)
+		self.holderName = holderName
+		self.createdAt = createdAt
+		self.updatedAt = updatedAt
+		self.billingAddress = Address()
     }
     
 }
@@ -148,12 +159,14 @@ extension Card {
                 if let jsonDictionary = jsonDictionary {
                     
                     if let cardListDictionary = jsonDictionary["data"] {
-                        
+							
                         let cardList = cardListDictionary as! [Dictionary<String,Any>]
+							
                         for cardDictionary in cardList {
-                            if let card = Card(fromDictionary: cardDictionary) {
-                                cards.append(card)
-                            }
+									
+									if let card = Card(fromDictionary: cardDictionary) {
+										cards.append(card)
+									}
                         }
                         completion(cards, nil)
                     }
--- Wallet/StringUtils.swift
+++ Wallet/StringUtils.swift
@@ -35,7 +35,7 @@ internal extension String {
 	var dateFromISO8601Format: Date? {
 		let dateFormatter = DateFormatter()
 		dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+		dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
 		return dateFormatter.date(from: self)
 	}
 }
