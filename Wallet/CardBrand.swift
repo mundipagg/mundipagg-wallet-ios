fa78bd3b86efc466bce31119d05c672f21a4ae54
Calebe Emerick <cemerik@stone.com.br>
2017-10-17 18:48:04 -0200

    Adicionando a bandeira JCB e mudando para lowercase.

--- Wallet/CardBrand.swift
+++ Wallet/CardBrand.swift
@@ -8,20 +8,56 @@
 
 import Foundation
 
-
-
-public enum CardBrand: String {
-	case Visa = "Visa"
-	case Mastercard = "Mastercard"
-	case Discover = "Discover"
-	case Elo = "Elo"
-	case Ticket = "Ticket"
-	case Alelo = "Alelo"
-	case Amex = "Amex"
-	case Aura = "Aura"
-	case DinersClub = "Diners"
-	case Sodexo = "Sodexo"
-    case Hipercard = "Hipercard"
-    case VR = "VR"
-	case Unknown = ""
+public enum CardBrand : String {
+	
+	public init(rawValue: String) {
+		
+		let brand = rawValue.lowercased()
+		
+		switch brand {
+		case "visa":
+			self = .visa
+		case "mastercard":
+			self = .mastercard
+		case "discover":
+			self = .discover
+		case "elo":
+			self = .elo
+		case "ticket":
+			self = .ticket
+		case "alelo":
+			self = .alelo
+		case "amex":
+			self = .amex
+		case "aura":
+			self = .aura
+		case "diners":
+			self = .dinersClub
+		case "sodexo":
+			self = .sodexo
+		case "hipercard":
+			self = .hipercard
+		case "vr":
+			self = .vr
+		case "jcb":
+			self = .jcb
+		default:
+			self = .unknown
+		}
+	}
+	
+	case visa = "visa"
+	case mastercard = "mastercard"
+	case amex = "amex"
+	case elo = "elo"
+	case discover = "discover"
+	case aura = "aura"
+	case dinersClub = "diners"
+	case hipercard = "hipercard"
+	case jcb = "jcb"
+	case sodexo = "sodexo"
+	case alelo = "alelo"
+	case vr = "vr"
+	case ticket = "ticket"
+	case unknown = ""
 }
